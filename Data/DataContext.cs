9477f0bf9b52916c793671373fe68ba15c8982b1
kroli98 <roli25321@gmail.com>
2023-02-22 17:24:29 +0100

    end of section 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingAppAPI.Entities;
+
+namespace DatingAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) {
+
+
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+            
+
+
+        
+
+    }
+}
---
+++ Entities/AppUser.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+
+using Microsoft.AspNetCore.Authentication;
+
+namespace DatingAppAPI.Entities
+{ 
+
+
+
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
